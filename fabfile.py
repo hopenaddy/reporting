f868cb38f127d783365484e88a59405c4a4bb64e
hopenaddy <na_5_ve@ukr.net>
2014-11-03 17:00:56 +0200

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -4,11 +4,11 @@ from fabvenv import Venv
 ROOT = "/opt/lv128/reporting"
 def deploy():
 	venv = Venv(ROOT, "requirements.txt")
-	if not venv.exists():
-		venv.create()
-		venv.install()
-put("manage.py", ROOT)
-put("lv128.service", ROOT)
-run("sudo mv %s/lv128.service /etc/systemd/system/" % ROOT)
-run("sudo systemctl enable lv128")
-run("sudo systemctl restart lv128")
+    if not venv.exists():
+        venv.create()
+    venv.install()
+    put("manage.py", ROOT)
+    put("lv128.service", ROOT)
+    run("sudo mv %s/lv128.service /etc/systemd/system/" % ROOT)
+    run("sudo systemctl enable lv128")
+    run("sudo systemctl restart lv128")
