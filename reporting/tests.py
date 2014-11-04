e8f3d53dbcc44c8035054b222efd4d19d8a27155
hopenaddy <na_5_ve@ukr.net>
2014-11-04 10:24:26 +0200

    Update tests.py

--- reporting/tests.py
+++ reporting/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import unittest
+#
+#
+class IndexPageTest(TestCase):
+#
+    def test_indexpage_available(self):
+        c = Client()
+        response = c.get('http://127.0.0.1:8000/', follow=True) 
+        self.assertEquals(response.status_code,200)
+      
+
+
+if __name__ == '__main__':
+	
+	unittest.main()
 
 # Create your tests here.
